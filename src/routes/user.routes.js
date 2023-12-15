1520556013f882e980edbc24cf324e21b5c16562
Radhika <radhikakumari0508@gmail.com>
2023-12-15 11:55:48 +0530

    Add access aefresh token, middleware and cookies

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 
 
@@ -19,5 +19,9 @@ router.route("/register").post(
     registerUser
     )
 
+router.route("/login").post(loginUser)  
+
+// secure routes
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router
