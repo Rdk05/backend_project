be48b2532978f07e772bddb73187e2d7d578a58c
Radhika <radhikakumari0508@gmail.com>
2023-12-23 21:05:46 +0530

    solve error

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -250,7 +250,9 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
 const getCurrentUser = asyncHandler(async(req, res) => {
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully");
+    .json(new ApiResponse(
+        200, req.user, "current user fetched successfully"
+    ));
 })
 
 const updateAccountDetails = asyncHandler(async(req, res) => {
@@ -260,7 +262,7 @@ const updateAccountDetails = asyncHandler(async(req, res) => {
         throw new ApiError(400, "All fields are required");
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
